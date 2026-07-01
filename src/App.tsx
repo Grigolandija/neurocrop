f623d0d2c000e2fc1cbd00a09dbd001c50cde3fe
Grigolandija <agrigas1@gmail.com>
2026-07-01 13:30:32 +0300

    t

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import './App.css'
 
 function App() {
-  return <iframe className="dashboard-frame" title="NeuroCrop Control Center" src="./dashboard.html" />
+  return (
+    <main className="app-shell">
+      <iframe className="dashboard-frame" title="NeuroCrop Control Center" src="./dashboard.html" />
+    </main>
+  )
 }
 
 export default App
