a3f63fa317a94a5816040cdf7269be5d4b787265
Grigolandija <agrigas1@gmail.com>
2026-07-02 11:33:20 +0300

    UPD

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{ts,tsx,html}'],
+  content: [
+    './index.html',
+    './src/**/*.{ts,tsx,html}',
+    './public/approved-dashboard-runtime.js',
+  ],
   theme: {
     extend: {
       fontFamily: {
